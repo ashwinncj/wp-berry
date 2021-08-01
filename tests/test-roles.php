cbaf13d3f7567d5abe962a964f8498ad25fb06f1
Ashwin Kumar <ashwinncj@gmail.com>
2021-08-01 18:14:47 +0530

    f/14: Updated test files.

---
+++ tests/test-roles.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wp_Berry
+ */
+
+use WPBerry\Settings;
+
+class RolesTest extends WP_UnitTestCase {
+
+	/**
+	 * Test to berify that administrator users are `berry_admin` and editors are `berr_user`
+	 */
+	public function testRolesAreAppledToAdminAndEditor() {
+
+		$admin = get_role( 'administrator' );
+		$this->assertTrue( $admin->has_cap( 'berry_admin' ) );
+
+		$editor = get_role( 'editor' );
+		$this->assertTrue( $editor->has_cap( 'berry_user' ) );
+
+	}
+}
